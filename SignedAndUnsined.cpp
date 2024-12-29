0f1fd38dd9f5e686d7061931a6927c455e8fae6d
shivamrajput18 <shivamsingh80094@gmail.com>
2024-12-29 19:12:01 +0530

    Add : data files

---
+++ SignedAndUnsined.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+      
+  unsigned int num = -1;
+  int x = num;
+  int k = 4294967295;
+  cout << num << endl;
+  cout << x << endl;
+  cout << k;
+
+
+} 
+
+
---
+++ TypeCasting.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    double st = 100.89;
+    int ans = st;
+    cout << ans << endl;
+
+    
+}
